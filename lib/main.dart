13445ea1bc6141a3508014be660bc851db4b9299
doomed-phobos <the_ekj.2121@outlook.com>
2026-03-26 14:22:38 -0500

    Added Database initialization and refactorized add page

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,18 @@ import 'package:tagger/database.dart';
 import 'package:tagger/theme.dart';
 import 'package:toastification/toastification.dart';
 import 'package:tagger/home_page.dart' as home;
-import 'package:tagger/add_page.dart' as add;
+import 'package:tagger/add_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Database.make_from_data()
-  .match(
-    () => runApp(const MaterialApp(home: Text("Failed to initialize Database!!"))),
-    (database) => runApp(App(database))
-  )
-  .run();
+  runApp(
+    await Database.make_from_data()
+    .match(
+      () => const MaterialApp(home: Text("Failed to initialize Database!!")),
+      (database) => App(database)
+    ).run()
+  );
 }
 
 class App extends StatelessWidget {
@@ -33,7 +34,7 @@ class App extends StatelessWidget {
             child: SafeArea(
               child: PageView(
                 controller: controller,
-                children: [buildHomePage(), buildAddPage()],
+                children: [buildHomePage(), AddPage()],
               ),
             ),
           ),
@@ -54,21 +55,6 @@ class App extends StatelessWidget {
         ),
         SizedBox(height: 10),
         // home.ArtistItem(test_artist),
-      ],
-    );
-  }
-
-  Widget buildAddPage() {
-    return Column(
-      crossAxisAlignment: .stretch,
-      children: [
-        const Text(
-          "Add",
-          textAlign: .center,
-          style: TextStyle(fontWeight: .bold, fontSize: 24),
-        ),
-        SizedBox(height: 10),
-        add.TagForm(),
       ],
     );
   }
