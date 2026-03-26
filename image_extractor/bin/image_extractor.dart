abfda28cef41e630dffb9adfddce1cb0a2fad64c
doomed-phobos <the_ekj.2121@outlook.com>
2026-03-26 09:00:03 -0500

    Imported image extractor in flutter project

--- image_extractor/bin/image_extractor.dart
+++
@@ -1,8 +0,0 @@
-import 'package:image_extractor/image_extractor.dart' as image_extractor;
-
-void main(List<String> arguments) async {
-  final task = image_extractor.get_image_url_from_hitomi_url("https://hitomi.la/reader/3855946.html#2-3");
-  final o = task.run();
-
-  print(await o);
-}
